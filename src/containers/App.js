b4ffbd07f5974397194c1d7ff44bc9180d2d3356
buentellopc <buentellopc@gmail.com>
2022-09-19 11:21:15 -0500

    Using fetch to get data from github API

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../components/Header/Header";
 import logo from "../logo.svg";
 import "./App.css";
+import Profile from "./Profile";
 
 function App() {
   return (
     <div className="App">
       <Header logo={logo} />
+      <Profile />
     </div>
   );
 }
